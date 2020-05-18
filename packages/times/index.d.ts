4f160bace07aa2f32d4b14ab21c6b75713ba2802
J.T <inbox@jurijtokarski.com>
2020-05-18 15:35:14 +0200

    = prepack

--- packages/times/index.d.ts
+++ packages/times/index.d.ts
@@ -2,3 +2,4 @@ export declare const getSecondsInMS: (x: number) => number;
 export declare const getMinutesInMS: (x: number) => number;
 export declare const getHoursInMS: (x: number) => number;
 export declare const getDaysInMS: (x: number) => number;
+export declare const getWeeksInMS: (x: number) => number;
