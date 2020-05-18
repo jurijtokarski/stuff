38425201630d2e16d497a2441e5b46729546f9ae
J.T <inbox@jurijtokarski.com>
2020-05-18 15:33:52 +0200

    = update times

--- packages/times/index.ts
+++ packages/times/index.ts
@@ -1,5 +1,9 @@
 export const getSecondsInMS = (x: number): number => x * 1000;
+
 export const getMinutesInMS = (x: number): number => getSecondsInMS(x * 60);
+
 export const getHoursInMS = (x: number): number => getMinutesInMS(x * 60);
+
 export const getDaysInMS = (x: number): number => getHoursInMS(x * 24);
+
 export const getWeeksInMS = (x: number): number => getDaysInMS(x * 7);
