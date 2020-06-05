bf26f5989efe6929685cac1a7d83b6061532c98e
J.T <inbox@jurijtokarski.com>
2020-06-05 17:22:50 +0200

    update

--- packages/format/index.d.ts
+++ packages/format/index.d.ts
@@ -1,1 +1,2 @@
 export declare function formatPrice(value: number, currency: string, locale?: string): string;
+export declare function capitalize(str: string): string;
--- packages/format/index.js
+++ packages/format/index.js
@@ -1,7 +1,11 @@
 "use strict";
 exports.__esModule = true;
-exports.formatPrice = void 0;
+exports.capitalize = exports.formatPrice = void 0;
 function formatPrice(value, currency, locale) {
     return new Intl.NumberFormat(locale || (navigator === null || navigator === void 0 ? void 0 : navigator.language) || "", { style: "currency", currency: currency }).format(value);
 }
 exports.formatPrice = formatPrice;
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+exports.capitalize = capitalize;
